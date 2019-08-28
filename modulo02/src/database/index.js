e28afb916d217a37a7d8f40c6e6ad27304072cfd
Camila Satie <camilasatie.dev@gmail.com>
2019-08-28 13:52:21 -0300

    subindo arquivos do modulo02

---
+++ modulo02/src/database/index.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+
+// importar os models
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
